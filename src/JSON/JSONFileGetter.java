6cae8239a542a045490408e3844541eb8c394b87
Joseph Lin <josephml@usc.edu>
2014-04-10 15:59:07 -0700

    Committing a REAL java file instead of a class.

---
+++ src/JSON/JSONFileGetter.java
@@ -0,0 +1,38 @@
+package mapGUI;
+
+import java.io.*;
+import java.net.*;
+
+// Referenced classes of package mapGUI:
+//            JSONFileParser
+
+public class JSONFileGetter
+{
+
+    public JSONFileGetter()
+    {
+        try
+        {
+            URL url = new URL("http://www-scf.usc.edu/~csci201/mahdi_project/test.json");
+            URLConnection connection = url.openConnection();
+            java.io.InputStream is = connection.getInputStream();
+            BufferedReader in = new BufferedReader(new InputStreamReader(is));
+            String JSONfile = in.readLine();
+            new JSONFileParser(JSONfile);
+        }
+        catch(MalformedURLException mue)
+        {
+            System.out.println((new StringBuilder("MUE:")).append(mue.getMessage()).toString());
+        }
+        catch(IOException ioe)
+        {
+            System.out.println((new StringBuilder("IOE:")).append(ioe.getMessage()).toString());
+        }
+    }
+
+    public static void main(String args[])
+    {
+        new JSONFileGetter();
+        System.out.println("HELLO WORLD");
+    }
+}
