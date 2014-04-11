ebc5b728c1c8900a3a13534931433d205dafc56f
Joseph Lin <josephml@usc.edu>
2014-04-11 13:18:21 -0700

    Deleted temp Main. Using UserInterface with 3 Tabs in it. Will be implementing it. The error was a local error with build paths and such.

--- src/main/GUI/UserInterface.java
+++ src/main/GUI/UserInterface.java
@@ -3,22 +3,33 @@ package main.GUI;
 import java.awt.BorderLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 
 public class UserInterface extends JPanel {
 	public UserInterface()
 	{
+		setSize(10000,10000);
 		JTabbedPane ThreeOpts = new JTabbedPane();
-		/*ThreeOpts.add("Set Route",); //Add the name of the JPanel of the chart);
-		ThreeOpts.add("Chart", );//Add the name of the JPanel);
-		ThreeOpts.add("Graph", );//Add the name of the JPanel);*/
+		JPanel SetRoute = new JPanel();
+		SetRoute.add();
+		SetRoute.setSize(200, 200);
+		ThreeOpts.addTab("Set Route", SetRoute); //Add the name of the JPanel of the chart);
+		JPanel DisplayChart = new JPanel();
+		
+		ThreeOpts.addTab("Chart", DisplayChart);//Add the name of the JPanel);
+		JPanel DisplayGraph = new JPanel();
+		ThreeOpts.addTab("Graph", DisplayGraph);//Add the name of the JPanel);
 		add(ThreeOpts, BorderLayout.NORTH);
 		
 	}
-	public static void main2(String[] args)
+	public static void main(String[] args)
 	{
 		JFrame a = new JFrame();
-		a.add(new UserInterface());
+		a.setSize(1000, 1000);
+		a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		a.add(new UserInterface(), BorderLayout.EAST);
+		a.setVisible(true);
 	}
 }
