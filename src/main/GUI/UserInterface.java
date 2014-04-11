9118ec3f74ff1ac156cbdfe4619b40ebdff2c253
Joseph Lin <josephml@usc.edu>
2014-04-10 19:54:34 -0700

    Adding a UserInterface. This will be the general JPanel on the right side. You will be adding the JPanels INTO the JTabbedPanes.

---
+++ src/main/GUI/UserInterface.java
@@ -0,0 +1,18 @@
+package main.GUI;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class UserInterface extends JPanel {
+	public UserInterface()
+	{
+		JTabbedPane ThreeOpts = new JTabbedPane();
+		/*ThreeOpts.add("Set Route",); //Add the name of the JPanel of the chart);
+		ThreeOpts.add("Chart", );//Add the name of the JPanel);
+		ThreeOpts.add("Graph", );//Add the name of the JPanel);*/
+		add(ThreeOpts, BorderLayout.NORTH);
+		
+	}
+}
