b81c6a61ab1276a0232337850db7f9a207be90db
Joseph Lin <josephml@usc.edu>
2014-04-11 23:24:13 -0700

    Whoo, finally figured out the merge fuck up. Working everything.

--- src/main/map/JMapViewerTest.java
+++ src/main/map/JMapViewerTest.java
@@ -1,3 +1,8 @@
+
+package main.map;
+
+
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -7,34 +12,38 @@ import javax.swing.JFrame;
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.openstreetmap.gui.jmapviewer.JMapViewer;
 import org.openstreetmap.gui.jmapviewer.MapMarkerCircle;
-import org.openstreetmap.gui.jmapviewer.Style;
 import org.openstreetmap.gui.jmapviewer.interfaces.MapMarker;
 
 
 public class JMapViewerTest extends JFrame {
 	final double markersize = .01;
 	final double maplat = 34.05;
-	final double maplon =  118.25;
-	public static void main(String[] args) 
-	{
-		new JMapViewerTest();
-	}
+	final double maplon = -118.25;
 	public JMapViewerTest() {
 		super("JMapViewerTest");
 		this.setSize(new Dimension(600, 600));
 
 		JMapViewer map = new JMapViewer();
-		Coordinate losangeles = new Coordinate(34.05, 118.25);
 		//map.setCenter(new Point(300,300));
-		//0, 0 is london.  x is north,   y is east
+		//0, 0 is london. +x is north, + y is east
 		System.out.println(JMapViewer.MAX_ZOOM);
-		map.setDisplayPositionByLatLon(34.05,  118.25, 12);
-		map.addMapMarker(new MapMarkerCircle(34.05,  118.25, .01));
 		map.setDisplayPositionByLatLon(maplat, maplon, 12);
 		map.addMouseListener(new PointListener(map));
 		//map.setDisplayPosition(1500,3000, 5);
-
+		MapMarkerCircle circle = new MapMarkerCircle(34.05, -118.25, .0005);
+		circle.setColor(Color.RED);
+		circle.setBackColor(Color.GREEN);
+		
+		map.addMapMarker(circle);
+		
 		add(map);
+
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+
+	public static void main(String[] args) {
+		new JMapViewerTest();
 	}
 	class PointListener implements MouseListener
 	{
@@ -50,19 +59,19 @@ public class JMapViewerTest extends JFrame {
 
 		@Override
 		public void mouseEntered(MouseEvent e) {
-			// TODO Auto generated method stub
+			// TODO Auto-generated method stub
 
 		}
 
 		@Override
 		public void mouseExited(MouseEvent e) {
-			// TODO Auto generated method stub
+			// TODO Auto-generated method stub
 
 		}
 
 		@Override
 		public void mousePressed(MouseEvent e) {
-			// TODO Auto generated method stub
+			// TODO Auto-generated method stub
 
 		}
 
