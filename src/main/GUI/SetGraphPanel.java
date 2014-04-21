e50f824e69bcfbfe0ab5f403eb52514557f53532
Joseph Lin <josephml@usc.edu>
2014-04-20 18:21:53 -0700

    Update Graph a tiny bit. Not quite working. Works in Proportions.

--- src/main/GUI/SetGraphPanel.java
+++ src/main/GUI/SetGraphPanel.java
@@ -10,7 +10,6 @@ import javax.swing.JPanel;
 public class SetGraphPanel extends JPanel{
 	int width;
 	int height;
-	double timeinc;
 	double min = Double.MAX_VALUE;
 	double max = Double.MIN_VALUE;
 	ArrayList<Double> TimeVal;
@@ -18,6 +17,10 @@ public class SetGraphPanel extends JPanel{
 	int topCornery = 10;
 	int bottomCornerx = 10;
 	int rightCornerx;
+	int leftDashfirst;
+	int leftDashlast;
+	int inc; //This defines the increase in pixels when analyzing the next point.
+	double timeinc; //This represents the amount of time in between each y axis point
 	public SetGraphPanel(int width, int height)
 	{
 		this.width = width;
@@ -45,7 +48,7 @@ public class SetGraphPanel extends JPanel{
 		g.drawLine(bottomCornerx, topCornery, bottomCornerx, bottomCornery);
 		//Bottom Line
 		g.drawLine(bottomCornerx, bottomCornery, rightCornerx, bottomCornery);
-		int inc = (width-20)/24;
+		inc = (rightCornerx - bottomCornerx)/24;
 		//Increments along the bottom
 		for (int i = 0; i < 24; i++)
 		{
@@ -60,9 +63,8 @@ public class SetGraphPanel extends JPanel{
 			g.drawLine(bottomCornerx+(inc*(i+1)), bottomCornery-5, bottomCornerx+ (inc*(i+1)), bottomCornery+5);
 		}
 		//Increments along the left along with dashes
-		bottomCornery = (height/2);
-		inc = (bottomCornery - 10) / 23;
-		double timeinc = (max - min) / 23;
+		inc = (bottomCornery - topCornery) / 23;
+		timeinc = (max - min) / 23;
 		for (int i = 0 ; i < 24; i++)
 		{
 			double currentVal = (min + (timeinc * i));			
@@ -74,16 +76,30 @@ public class SetGraphPanel extends JPanel{
 				y = "" + currentVal;
 			g.drawString(y, -1, bottomCornery - (inc * i+1));
 			g.drawLine(5, bottomCornery - (inc * i+2), 15, bottomCornery - (inc * i+2));
+			if ( i == 0)
+				leftDashfirst = bottomCornery - (inc);
+			if (i == 23)
+				leftDashlast = bottomCornery - (inc * 25);
 		}
 		
 		for (int i = 1 ; i < TimeVal.size(); i++)
 		{
-			determineLine();
+			inc = (rightCornerx - bottomCornerx)/24;
+			determineLine(g, TimeVal.get(i), TimeVal.get(i-1), i);
 			//g.drawLine(x1, y1, x2, y2);
 		}
 	}
-	public void determineLine()
+	public void determineLine(Graphics g, Double p1, Double p2, int numberEntry)
 	{
 		
+		double ProportionUpP1 = p1/(max - min);
+		double ProportionUpP2 = p2/(max - min);
+		
+		double y1 = (leftDashfirst - leftDashlast) * (ProportionUpP1); 
+		double y2 = (leftDashfirst - leftDashlast) * (ProportionUpP2);
+		int point1y = (int) y1;
+		int point2y = (int) y2;
+//		g.drawLine(30, bottomCornerx + (numberEntry-1 * inc));
+		g.drawLine(0, leftDashlast, 100, leftDashlast);
 	}
 }
