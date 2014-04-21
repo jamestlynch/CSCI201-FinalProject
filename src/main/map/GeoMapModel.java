fe0a8c993b819be7639e556ea65070e5bcff77c8
Joseph Lin <josephml@usc.edu>
2014-04-20 23:27:55 -0700

    I added an isJunction functionality for fastest path usage.

--- src/main/map/GeoMapModel.java
+++ src/main/map/GeoMapModel.java
@@ -16,9 +16,9 @@ import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 
 import main.CSCI201Maps;
+import main.automobile.Automobile;
 import main.freeway.FreewayRamp;
 import main.freeway.FreewaySegment;
-import main.automobile.Automobile;
 
 import org.openstreetmap.gui.jmapviewer.Coordinate;
 import org.w3c.dom.Document;
@@ -155,7 +155,12 @@ public class GeoMapModel implements Runnable {
 		}
 		return null;
 	}
-	
+	public int isJunction(FreewaySegment oldSegment)
+	{
+		
+		return defaultDirectionFreewayNetwork.get(oldSegment.getEndRamp()).size();		
+			
+	}
 	public FreewaySegment getNextFreewaySegment(FreewaySegment oldSegment) {
 		//Checks to see if there is a following segment after this ramp (End of the Freeway)
 		
@@ -744,5 +749,6 @@ public class GeoMapModel implements Runnable {
 	 * in our HashMap that begins at the ramp passed in.
 	 * =========================================================================
 	 */
+
 
 }
