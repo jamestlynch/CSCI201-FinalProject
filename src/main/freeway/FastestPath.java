6bcfaff1b0fd8343b297c8b94107002b9da479b9
Ivana Wang <ivanawan@usc.edu>
2014-04-28 22:40:22 -0700

    to get rid of dirty work tree

--- src/main/freeway/FastestPath.java
+++ src/main/freeway/FastestPath.java
@@ -24,12 +24,13 @@ public class FastestPath {
 	private static ArrayList<FreewaySegment> fastestPath = new ArrayList<FreewaySegment>();
 	public double path1time = 0;
 	public double path2time = 0;
+	public double fastestPathTime = 0;
 	
-	public double findFastestPath(String start, String end, GeoMapModel mapModel){
+	public ArrayList<FreewaySegment> findFastestPath(String start, String end, GeoMapModel mapModel){
 		this.source = start;
 		this.destination = end;
 		
-		//find a FreewaySegment that the source & destination belongs to
+		//find a FreewaySegment that the source & destination belongs to (arbitrarily chosen boolean)
 		sourceFreewaySegment = mapModel.searchByRampName(source, true);	
 		destinationFreewaySegment = mapModel.searchByRampName(destination, true);
 		//read in freeway names
@@ -44,19 +45,18 @@ public class FastestPath {
 				path2time += currFreewaySegment.getAverageSpeed();
 				path2.add(currFreewaySegment);
 				currFreewaySegment = mapModel.getNextFreewaySegment(currFreewaySegment);
-			//========================================================
+		//========================================================
 		
-		//CASE 1: Source is on the 10
+		//CASE 1: Source is on a middle segment		
+				
 		if(sourceFreewayName == "10"){
-			//TODO Case A: Source is on left nubbin segment
+			//Case A: Source is on end segment
 			currFreewaySegment = sourceFreewaySegment;
-			if(mapModel.isJunction(currFreewaySegment) == 0 && sourceFreewaySegment.getSegmentName() == " "){
-				//Go right, continue with path
-				//Go right, then down 405
-			}
+			MoveAlongExtraSection(currFreewaySegment, fastestPath, fastestPathTime, mapModel);
+			NavigateJunction(currFreewaySegment, mapModel);
 			
 			//TODO Case B: Source is on the right nubbin segment
-			else if(mapModel.isJunction(currFreewaySegment) == 0 && sourceFreewaySegment.getSegmentName() == " "){
+			(mapModel.isJunction(currFreewaySegment) == 0 && sourceFreewaySegment.getSegmentName() == " "){
 				sourceFreewaySegment = mapModel.searchByRampName(source, false);
 				destinationFreewaySegment = mapModel.searchByRampName(destination, false);
 				currFreewaySegment = sourceFreewaySegment;
@@ -64,7 +64,7 @@ public class FastestPath {
 				//Go left, down 101 
 			}
 			
-			else{
+			if{
 			//Case C: Source is in middle
 				//Start by going right
 				sourceFreewaySegment = mapModel.searchByRampName(source, true);
@@ -86,21 +86,6 @@ public class FastestPath {
 					currFreewaySegment = mapModel.getNextFreewaySegment(currFreewaySegment);
 					startRamp = currFreewaySegment.getStartRamp();
 				}
-				/* 	=============FOR REFERENCE================	
-				 * go right until you hit a junction
-				 * 	go down 101
-				 * 	if(destinationFreewayName == 101)
-				 * 		stop at destination
-				 * 	else go until you hit a junction
-				 * 	go left on 105
-				 * 	if(destinationFreewayName == 105)
-				 * 		stop at destination
-				 * 	else go until you hit a junction
-				 * 	go up 405
-				 * 	if(destinationFreewayName == 405)
-				 * 		stop at destination
-				 * 	path1speed = sum of all average speeds of segments on path 
-				 */
 			
 			//Start by going left
 				sourceFreewaySegment = mapModel.searchByRampName(source, false);
@@ -122,22 +107,7 @@ public class FastestPath {
 					currFreewaySegment = mapModel.getNextFreewaySegment(currFreewaySegment);
 					startRamp = currFreewaySegment.getStartRamp();
 				}
-				/* ==========FOR REFERENCE===========
-				 * go left until you hit a junction
-				 * 	go down 405
-				 *	if(destinationFreewayName == 405)
-				 * 	stop at destination
-			 	*	else go until you hit a junction
-			 	* 	go right on 105
-			 	* 	if(destinationFreewayName == 105)
-			 	* 		stop at destination
-			 	* 	else go until you hit a junction
-			 	* 	go up 101
-			 	* 	if(destinationFreewayName == 101)
-			 	* 		stop at destination
-			 	* 	path2speed = sum of all average speeds of segments on path
-				 */
-			}
+			
 			
 		}//end 10 freeway case
 		
@@ -200,128 +170,63 @@ public class FastestPath {
 				}
 			}
 			
-		}//end 105 case
-			/*
-	
-			 * 
-			 * if(sourceFreewayName == the 105)
-			 * case a:
-			 * 		go right until you hit a junction
-			 * 		go up 101
-			 * 		if(destinationFreewayName == 101)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go left on 10
-			 * 		if(destinationFreewayName == 10)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go down 405
-			 * 		if(destinationFreewayName == 405)
-			 * 			stop at destination
-			 * 		path1speed = sum of all average speeds of segments on path
-			 * case b:
-			 * 		go left until you hit a junction
-			 * 		go up 405
-			 * 		if(destinationFreewayName == 405)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go right on 10
-			 * 		if(destinationFreewayName == 10)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go down 101
-			 * 		if(destinationFreewayName == 101)
-			 * 			stop at destination
-			 * 		path2speed = sum of all average speeds of segments on path
-			 * 
-			 * if(sourceFreewayName == the 101)
-			 * case a:
-			 * 		go up until you hit a junction
-			 * 		go left on 10
-			 * 		if(destinationFreewyName == 10)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go down on 405
-			 * 		if(destinationFreewayName == 405)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go right on 105
-			 * 		if(destinationFreewayName == 105)
-			 * 			stop at destination
-			 * 		path1speed = sum of all average speeds of segments on path
-			 * case b:
-			 * 		go down until you hit a junction
-			 * 		go left on 105
-			 * 		if(destinationFreewayName == 105)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go up on 405
-			 * 		if(destinationFreewayName == 405)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go right on 10
-			 * 		if(destinationFreewayName == 10)
-			 * 			stop at destination
-			 * 		path2speed = sum of all average speeds of segments on path
-			 * 
-			 *  if(sourceFreewayName == the 405)
-			 * case a:
-			 * 		go up until you hit a junction
-			 * 		go right on 10
-			 * 		if(destination is on 10)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go down on 101
-			 * 		if(destination is on 101)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go left on 105
-			 * 		if(destination is on 105)
-			 * 			stop at destination
-			 * 		path1speed = sum of all average speeds of segments on path
-			 * case b:
-			 * 		go down until you hit a junction
-			 * 		go right on 105
-			 * 		if(destination is on 105)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go up on 101
-			 * 		if(destination is on 101)
-			 * 			stop at destination
-			 * 		else go until you hit a junction
-			 * 		go left on 10
-			 * 		if(destination is on 10)
-			 * 			stop at destination
-			 * 		path2speed = sum of all average speeds of segments on path
-			 */
-			
-		
+		}//end 105 case		
 		
 		//Compare the 2 speeds
 		if(path1time > path2time){
-			return path1time;
+			fastestPath = path1;
 		}
 		else{
-			return path2time;
+			fastestPath = path2;
+		}
+		return fastestPath;
+		
+	}
+	
+	//Move along freeway in middle until it reaches a junction
+	public void MoveAlongMidSection(FreewaySegment currentSegment, ArrayList<FreewaySegment> path, double pathTime, GeoMapModel mapModel){
+		startRamp = currentSegment.getStartRamp();
+		while (startRamp.getRampName() != destination){
+			//Goes until right before a junction
+			if(mapModel.isJunction((mapModel.getNextFreewaySegment(currFreewaySegment))) == 2){
+				NavigateJunction(currentSegment, mapModel);
+			}
+			pathTime += currFreewaySegment.getAverageSpeed();
+			path.add(currFreewaySegment);
+			currFreewaySegment = mapModel.getNextFreewaySegment(currFreewaySegment);
+			startRamp = currFreewaySegment.getStartRamp();
+		}
+	}
+	
+	//Move along an end segment
+	public void MoveAlongExtraSection(FreewaySegment currentSegment, ArrayList<FreewaySegment> path, double pathTime, GeoMapModel mapModel){
+		//move until junction
+		while(startRamp.getRampName() != destination){
+			pathTime += currentSegment.getAverageSpeed();
+			path.add(currentSegment);
+			currentSegment = mapModel.getNextFreewaySegment(currentSegment);
+		}
+		//transition to middle section
+	}
+	
+	//At a junction
+	public void NavigateJunction(FreewaySegment currFreewaySegment, GeoMapModel mapModel){
+		//Case a: Keep going on current freeway
+		currFreewaySegment = mapModel.getNextFreewaySegment(currFreewaySegment);
+		//Leads to an end segment
+		if(mapModel.isJunction(currFreewaySegment) == 0){
+			//destination is on end segment
+			if(currFreewaySegment.getFreewayName() == destinationFreewayName){
+				path1time += currFreewaySegment.getAverageSpeed();
+				path1.add(currFreewaySegment);
+			}
 		}
 		
-	}
-	
-	//TODO Historical Fastest Path
 
-	/*  POSSIBLY NEATER TO USE THIS FUNCTION
-	//finds which freeway the given location belongs to
-	public void findFreeways(FreewaySegment start, FreewaySegment end){
-		//search in list of ramps for match
+		//Case b: Transition to next freeway
 		
-		sourceFreeway = start.getFreewayName();
-		destinationFreeway = end.getFreewayName();
+		
+		//Case c: Transition to next freeway, but backwards
 	}
-	*/
 	
-	public static void main(String args[]){
-		//Draw Fastest Path
-			//TODO
-//		mapview.draw(fastestPath);
-	}
 }
